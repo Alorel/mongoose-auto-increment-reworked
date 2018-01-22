61ef00bc1d6cbdd97374f5547571aed6a3a5ccd7
Arturas Molcanovas <a.molcanovas@gmail.com>
2018-01-22 01:34:39 +0000

    test different mongoose versions

---
+++ build/set-mongoose-version.js
@@ -0,0 +1,27 @@
+if (process.env.MONGOOSE) {
+  const ver = parseInt(process.env.MONGOOSE, 10);
+  const pkg = require('../package.json');
+  
+  switch (ver) {
+    case 4:
+      pkg.peerDependencies.mongoose = '^4.0.0';
+      pkg.devDependencies.mongoose = '^4.0.0';
+      break;
+    case 5:
+      pkg.peerDependencies.mongoose = '^5.0.0';
+      pkg.devDependencies.mongoose = '^5.0.0';
+      break;
+    default:
+      console.log('Skipping: MONGOOSE env variable invalid: ' + process.env.MONGOOSE);
+      process.exit(0);
+  }
+  
+  const fs = require('fs');
+  const path = require('path');
+  fs.writeFileSync(path.resolve(__dirname, '../package.json'), JSON.stringify(pkg, null, 2));
+  
+  const util = require('util');
+  console.log(util.inspect(pkg, {colors: true}))
+} else {
+  console.log('Skipping: MONGOOSE env variable absent or unsupported');
+}
